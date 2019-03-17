e9e04ad233f1b9e7daaab6b60762249eb83da449
riri-jp <alahmary.mashari@gmail.com>
2019-03-17 15:04:16 +0300

    login

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -9,11 +9,48 @@ const instance = axios.create({
   baseURL: "https://api-chatr.herokuapp.com/"
 });
 
-const setAuthToken = token => {};
+const setAuthToken = token => {
+  if (token) {
+    localStorage.setItem("token", token);
+    axios.defaults.headers.common.Authorization = `jwt ${token}`;
+  } else {
+    localStorage.removeItem("token");
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
 
-export const checkForExpiredToken = () => {};
+export const checkForExpiredToken = () => {
+  return dispatch => {
+    const token = localStorage.getItem("token");
 
-export const login = userData => {};
+    if (token) {
+      const currentTime = Date.now() / 1000;
+
+      const user = jwt_decode(token);
+
+      if (user.exp >= currentTime) {
+        setAuthToken(token);
+        dispatch(setCurrentUser(user));
+      } else {
+        dispatch(logout());
+      }
+    }
+  };
+};
+
+export const login = (userData, history) => {
+  return async dispatch => {
+    try {
+      let response = await instance.post("/login/", userData);
+      let user = response.data;
+      setAuthToken(user.token);
+      dispatch(setCurrentUser(jwt_decode(user.token)));
+      history.push("/channels");
+    } catch (error) {
+      console.error(error.response.data);
+    }
+  };
+};
 
 export const signup = userData => {};
 
